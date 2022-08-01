c9b747c6d8a82d457f19623eedc0536383723971
LH <me@lholota.com>
2022-08-01 17:06:46 +0200

    feat: First version

---
+++ root/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env ash
+
+# Ensure the config directory exists
+mkdir -p /config/qBittorrent
+
+# Copy default config if none exists
+if [[ ! -e /config/qBittorrent/qBittorrent.conf ]]
+then
+    cp /defaults/qBittorrent.conf /config/qBittorrent/qBittorrent.conf
+fi
+
+/usr/bin/qbittorrent-nox --webui-port=8080
